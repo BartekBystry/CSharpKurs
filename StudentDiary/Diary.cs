69eebe9021e6195bfbebf25e31595467cfd91a5f
Bartek <bartlomiej@bystrzycki.com>
2020-09-22 18:19:20 +0200

    StudentDiary project added

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //State (variables)
+        List<float> ratings = new List<float>();
+
+        //Behavior
+        public void addRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float calculateAverage()
+        {
+            float sum = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+ 
+            return sum / ratings.Count();
+        }
+
+        public float getMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float getMinRating()
+        {
+            return ratings.Min();
+        }
+
+
+    }
+}
